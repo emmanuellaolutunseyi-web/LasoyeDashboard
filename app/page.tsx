d7965a9b737646c0706ea68af96597225f42746e
emmanuellaolutunseyi-web <emmanuellaolutunseyi@gmail.com>
2026-08-12 17:29:36 +0100

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,132 @@
 import Image from "next/image";
+import { LayoutDashboard, User, Settings, Settings2, TrendingUp, FileText, ChartColumn } from "lucide-react"
+function Navbar() {
+  return (
+    <nav className="flex justify-between items-center bg-black text-white py-4 px-6">
+      <div className="py-2 px-3 ml-3.5 bg-white text-slate-800 rounded-md italic font-semibold">
+        Lasoye
+      </div>
 
+      <ul className="flex gap-12 text-sm font-semibold ">
+        <li className="hover:translate-y-0.5 transition-all duration-200 cursor-pointer">Products</li>
+        <li className="hover:translate-y-0.5 transition-all duration-200 cursor-pointer">Our company</li>
+        <li className="hover:translate-y-0.5 transition-all duration-200 cursor-pointer">Account</li>
+        <li className="hover:translate-y-0.5 transition-all duration-200 cursor-pointer">Contact</li>
+      </ul>
+      <div className="space-x-4 mr-6">
+        <button className="bg-pink-400 font-medium py-2 px-3 cursor-pointer hover:bg-white hover:text-pink-400 transition-all duration-200">
+          Sign Up
+        </button>
+        <button className="bg-white font-medium text-black py-2 px-3 cursor-pointer hover:text-pink-400 transition-all duration-200">
+          Log In
+        </button>
+      </div>
+    </nav>
+  )
+}
+
+function Section(){
+  return (
+    <section className="grid grid-cols-[300px_1fr] gap-4 mt-14 mb-14 px-8 ">
+      <aside className="bg-black min-h-screen rounded-l-xl flex flex-col pt-4">
+        <div className="border border-t-0 border-b-white/25 py-3 px-2 ">
+          <div className="py-2 px-3 ml-4 bg-white text-slate-800 rounded-md italic font-semibold w-20">
+            Lasoye
+          </div>
+        </div>
+        {/* */}
+        <div className="py-2 px-6">
+          <h4 className="text-white/80 text-xs mb-3">
+            GENERAL
+          </h4>
+          <div className="text-white/80">
+            <div className="mb-2.5 px-4 py-2 hover:bg-white/10 hover:text-white transition-all duration-200 rounded-md">
+              <p className="text-sm">
+                <LayoutDashboard className="inline-block w-5 mr-3"/>
+                Dashboard
+              </p>
+            </div>
+            <div className="mb-4 px-4 py-2 hover:bg-white/10 hover:text-white transition-all duration-200 rounded-md">
+              <p className="text-sm">
+                <User className="inline-block w-5 mr-3"/>
+                My Account
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="py-2 px-6">
+          <h4 className="text-white/80 text-xs mb-3">
+            DEMO
+          </h4>
+          <div className="text-white/80">
+            <div className="mb-2.5 px-4 py-2 hover:bg-white/10 hover:text-white transition-all duration-200 rounded-md">
+              <p className="text-sm">
+                <FileText className="inline-block w-5 mr-3"/>
+                Demo 1
+              </p>
+            </div>
+            <div className="mb-2.5 px-4 py-2 hover:bg-white/10 hover:text-white transition-all duration-200 rounded-md">
+              <p className="text-sm">
+                <ChartColumn  className="inline-block w-5 mr-3"/>
+                Demo 2
+              </p>
+            </div>
+            <div className="mb-2.5 px-4 py-2 hover:bg-white/10 hover:text-white transition-all duration-200 rounded-md">
+              <p className="text-sm">
+                <FileText className="inline-block w-5 mr-3"/>
+                Case Management
+              </p>
+            </div>
+            <div className="mb-2.5 px-4 py-2 hover:bg-white/10 hover:text-white transition-all duration-200 rounded-md">
+              <p className="text-sm">
+                <Settings className="inline-block w-5 mr-3"/>
+                Demo 4
+              </p>
+            </div>
+            <div className="mb-2.5 px-4 py-2 hover:bg-white/10 hover:text-white transition-all duration-200 rounded-md">
+              <p className="text-sm">
+                <Settings className="inline-block w-5 mr-3"/>
+                Demo 5
+              </p>
+            </div>
+            <div className="mb-2.5 px-4 py-2 hover:bg-white/10 hover:text-white transition-all duration-200 rounded-md">
+              <p className="text-sm">
+                <ChartColumn className="inline-block w-5 mr-3"/>
+                Demo 6
+              </p>
+            </div>
+            <div className="mb-2.5 px-4 py-2 hover:bg-white/10 hover:text-white transition-all duration-200 rounded-md">
+              <p className="text-sm">
+                <FileText className="inline-block w-5 mr-3"/>
+                Demo 7
+              </p>
+            </div>
+            <div className="mb-2.5 px-4 py-2 hover:bg-white/10 hover:text-white transition-all duration-200 rounded-md">
+              <p className="text-sm">
+                <Settings className="inline-block w-5 mr-3"/>
+                Demo 8
+              </p>
+            </div>
+          
+          </div>
+
+        </div>
+        
+        
+      </aside>
+      <div>
+        lorem weqk\uhbqaduqwadbj
+        qAEGHA\
+        GEHAygwarhd
+      </div>
+    </section>
+  )
+}
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main className="min-h-screen ">
+      <Navbar />
+      <Section/>
+    </main>
   );
 }
